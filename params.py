20e2ff7e8ac50f96f91ddc72c92dabdb6ca7f2f6
arokem <arokem@gmail.com>
2013-08-05 18:06:48 -0700

    Initial commit

---
+++ params.py
@@ -0,0 +1,20 @@
+
+p = dict(
+    subject = 'XXX',
+    #Fixation size (in degrees):
+    fixation_size = 0.2,
+    monitor='TestMonitor',
+    scanner=True,
+    screen_number = 0,
+    full_screen = False,
+    radial_cyc = 10,
+    angular_cyc = 15,
+    angular_width=30,
+    size = 60, #This just needs to be larger than the screen
+    temporal_freq = 8,
+    sf = 10,
+    n_blocks = 30, #30 blocks = 300 sec = 5 minutes
+    block_duration=10,
+     color_dur = 1/3. # 2 Hz
+    
+    )
